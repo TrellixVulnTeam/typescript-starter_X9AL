9eb0b21116eaa318a71fb63487b634c1f4e8936f
Panayotis Silintziris <psilintziris@gmail.com>
2021-10-20 19:31:43 +0300

    fiex

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import _ from 'ramda';
+
+console.log('Hello');
+const add = (x) => x + 1;
+
+console.log(add(1, 2));
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('Hello world');
+import R from 'ramda';
+
+const enum CookieType {
+  FirstParty = '1st party',
+  ThirdParty = '3rd party',
+}
+
+const add(x: number) => x + 1;
+
+  console.log(CookieType.FirstParty);
+
+const COOKIE_TYPES = [CookieType.FirstParty, CookieType.ThirdParty];
+
+const foo = (type: CookieType): boolean => {
+  return type === CookieType.FirstParty;
+};
+
+console.log(foo(CookieType.FirstParty));
